cd623cffac30a6129d043dda55b7fdd2eb7e1c8c
Remsely <prokopchukromain@mail.ru>
2024-08-17 20:41:16 +0300

    fix: rename main class

--- back/app/src/main/kotlin/ru/remsely/psihosom/app/PsihosomApplication.kt
+++ back/app/src/main/kotlin/ru/remsely/psihosom/app/PsihosomApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AppApplication
+class PsihosomApplication
 
 fun main(args: Array<String>) {
-    runApplication<AppApplication>(*args)
+    runApplication<PsihosomApplication>(*args)
 }
