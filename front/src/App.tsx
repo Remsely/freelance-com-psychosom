828bc314f0ff69a138ae1691be3f7d88e9e21edb
Nikolay Polyakov <misterkola337@gmail.com>
2024-08-27 20:28:56 +0300

    feat: add header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,14 @@ export default function App() {
         <>
             <header className="container">
                 <h1 className="title">НАЗВАНИЕ</h1>
+                <nav className="navbar">
+                    <ul>
+                        <li><a href="">Главаня</a></li>
+                        <li><a href="">Отзывы</a></li>
+                        <li><a href="">О психосоматике</a></li>
+                        <li><a href="">Записаться на консультацию</a></li>
+                    </ul>
+                </nav>
             </header>
             <div className="main_container container">
 
