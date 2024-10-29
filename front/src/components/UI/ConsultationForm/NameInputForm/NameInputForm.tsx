225222b09a446044fb6224026de9d2ef6334abcc
Nikolay Polyakov <misterkola337@gmail.com>
2024-10-29 23:32:55 +0300

    fix: fix translate array

--- front/src/components/UI/ConsultationForm/NameInputForm/NameInputForm.tsx
+++ front/src/components/UI/ConsultationForm/NameInputForm/NameInputForm.tsx
@@ -13,10 +13,13 @@ interface NameInputProps {
 export default function NameInputForm({label, name, register, errors, clearErrors}: NameInputProps) {
     const translateToCyrillic = (input: string): string => {
         const translationMap: { [key: string]: string } = {
-            q: 'й', w: 'ц', e: 'у', r: 'к', t: 'е', y: 'н', u: 'г', i: 'ш', o: 'щ', p: 'з',
-            a: 'ф', s: 'ы', d: 'в', f: 'а', g: 'п', h: 'р', j: 'о', k: 'л', l: 'д',
-            z: 'я', x: 'ч', c: 'с', v: 'м', b: 'и', n: 'т', m: 'ь',
+            q: 'й', w: 'ц', e: 'у', r: 'к', t: 'е', y: 'н', u: 'г', i: 'ш', o: 'щ', p: 'з', "[": "х", "]": "ъ",
+            a: 'ф', s: 'ы', d: 'в', f: 'а', g: 'п', h: 'р', j: 'о', k: 'л', l: 'д', ";": "ж", "'": "э",
+            z: 'я', x: 'ч', c: 'с', v: 'м', b: 'и', n: 'т', m: 'ь', ".": "ю", ",": "б",
 
+            Q: 'Й', W: 'Ц', E: 'У', R: 'К', T: 'Е', Y: 'Н', U: 'Г', I: 'Ш', O: 'Щ', P: 'З', "{": "Х", "}": "Ъ",
+            A: 'Ф', S: 'Ы', D: 'В', F: 'А', G: 'П', H: 'Р', J: 'О', K: 'Л', L: 'Д', ":": "Ж", "\"": "Э",
+            Z: 'Я', X: 'Ч', C: 'С', V: 'М', B: 'И', N: 'Т', M: 'Ь', "<": "Ю", ">": "Б",
         };
 
         return input.split('').map(char => translationMap[char] || char).join('');
