e2d86494bd89988f9212d92581f9ae7b82c1c8d5
Nikolay Polyakov <misterkola337@gmail.com>
2024-10-29 21:05:04 +0300

    fix: fix margins and paddings in review form

--- front/src/components/UI/ReviewForm/StarRating/StarRating.tsx
+++ front/src/components/UI/ReviewForm/StarRating/StarRating.tsx
@@ -16,7 +16,6 @@ export default function StarRating({onRatingSelect}: StarRatingProps) {
 
     return (
         <div className={styles.row}>
-            <h2>Оценка</h2>
             <div className={styles.starRating}>
                 {[...Array(5)].map((_, index) => {
                     const starIndex = index + 1;
@@ -31,17 +30,18 @@ export default function StarRating({onRatingSelect}: StarRatingProps) {
                             onMouseLeave={() => setHover(0)}
                             aria-label={`Оценка ${starIndex} звезд`}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="auto" viewBox="0 0 24 24"
                                  fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                  stroke-linejoin="round" className={styles.starIcon}>
                                 <polygon
                                     points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
                             </svg>
                         </a>
-                )
-                    ;
+                    )
+                        ;
                 })}
             </div>
+            <h4 className={styles.title}>Оставьте свою оценку</h4>
         </div>
     );
 }
