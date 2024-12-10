0dbddbb8456cb4b4b5a5cc0f8caee12774084952
Nikolay Polyakov <misterkola337@gmail.com>
2024-12-10 20:19:24 +0300

    fix: fix errors for password inputs

--- front/shared/componetns/shared/Inputs/PasswordInput/PasswordInput.tsx
+++ front/shared/componetns/shared/Inputs/PasswordInput/PasswordInput.tsx
@@ -3,11 +3,12 @@
 import styles from "./PasswordInput.module.scss"
 import {useState} from "react";
 import {Eye, EyeOff} from "lucide-react";
-import {FieldError, FieldValues, UseFormRegister} from "react-hook-form";
+import {FieldError, FieldValues, UseFormRegister, UseFormWatch} from "react-hook-form";
 
 interface PasswordInputProps {
     register: UseFormRegister<FieldValues>;
     errors: Record<string, FieldError | undefined>;
+    watch: UseFormWatch<FieldValues>;
     mode?: string;
 }
 
@@ -23,18 +24,30 @@ export function PasswordInput(props: PasswordInputProps) {
             <div className={styles.row}>
                 <h2 className={styles.title}>{props.mode === "again" ? "Повторите пароль" : "Пароль"}</h2>
                 <div className={styles.input}>
-                    <input type={showPassword ? "text" : "password"} {...props.register("password", {
-                        required: "Это поле обязательное",
-                        pattern: {
-                            value: /^.{8,}$/,
-                            message: "Пароль должен быть не менее 8 символов",
-                        },
-                    })}/>
-                    {props.errors.password && <p className={styles.error}>{props.errors.password.message}</p>}
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        {...props.register(
+                            props.mode === "again" ? "passwordAgain" : "password",
+                            props.mode === "again"
+                                ? {
+                                    required: "Это поле обязательное",
+                                    validate: (value) =>
+                                        value === props.watch("password") || "Пароли не совпадают",
+                                }
+                                : {
+                                    required: "Это поле обязательное",
+                                    pattern: {
+                                        value: /^.{8,}$/,
+                                        message: "Пароль должен быть не менее 8 символов",
+                                    },
+                                }
+                        )}
+                    />
+                    {props.errors[props.mode === "again" ? "passwordAgain" : "password"] && (
+                        <p className={styles.error}>{props.errors[props.mode === "again" ? "passwordAgain" : "password"]?.message}</p>
+                    )}
                     <i className={styles.eye} onClick={toggleShowPassword}>
-                        {showPassword ?
-                            <EyeOff/> :
-                            <Eye/>}
+                        {showPassword ? <EyeOff /> : <Eye />}
                     </i>
                 </div>
             </div>
