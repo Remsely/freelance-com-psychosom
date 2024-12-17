f81c6fb127f9d56a8493372b94fa0bbe98fd2a88
Nikolay Polyakov <misterkola337@gmail.com>
2024-12-17 17:03:08 +0300

    fix: refactor placeholders

--- front/shared/componetns/shared/Header/Header.tsx
+++ front/shared/componetns/shared/Header/Header.tsx
@@ -2,16 +2,16 @@
 
 import styles from "./Header.module.scss"
 import {AuthButton, Navbar} from "@/shared/componetns/shared";
-import {useSession} from "next-auth/react";
+import Image from "next/image";
 
 export function Header() {
-    const {data: session} = useSession();
-    console.log(session, "999")
-
     return (
         <>
             <header id="head" className={`${styles.header} container`}>
-                <h1 className={styles.title}>{session ? "Авторизирован" : "Не авторизирован"}</h1>
+                <div className={styles.logo}>
+                    <Image src="/logo-without-bg.svg" alt="" width={50} height={50}/>
+                    <h1 className={styles.title}>ПсихоСома</h1>
+                </div>
                 <nav className={styles.navbar}>
                     <Navbar/>
                     <div className={styles.authButton}>
