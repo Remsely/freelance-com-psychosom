ed8d6a68b099eda4fa7905a4f1f197c7e1564421
Nikolay Polyakov <misterkola337@gmail.com>
2025-02-04 15:06:05 +0300

    feat: add normalize.scss, add profile and not-auth pages and fix others bugs

---
+++ front/app/profile/page.tsx
@@ -0,0 +1,19 @@
+import { getUserSession } from '@/shared/lib/get-user-session';
+import { redirect } from 'next/navigation';
+
+export default async function ProfilePage() {
+    const session = await getUserSession();
+
+    if (!session) {
+        return redirect('/not-auth');
+    }
+
+    const user = session;
+
+
+    if (!user) {
+        return redirect('/not-auth');
+    }
+
+    return <div>{user.firstname}</div>;
+}
